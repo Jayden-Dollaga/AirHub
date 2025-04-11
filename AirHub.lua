fbaa5a78437846d058a23bd38d7bb1f087f8edcb
Jayden-Dollaga <jaydendollaga4@gmail.com>
2025-04-12 01:24:39 +0800

    Update AirHub.lua
    asd

--- AirHub.lua
+++ AirHub.lua
@@ -22,8 +22,8 @@ getgenv().AirHub = {}
 
 --// Load Modules
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/Modules/Aimbot.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/Modules/Wall%20Hack.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayden-Dollaga/AirHub/main/Modules/Aimbot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayden-Dollaga/AirHub/main/Modules/Wall%20Hack.lua"))()
 
 --// Variables
 
